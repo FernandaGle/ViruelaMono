d47df5c097658072dd11dc535772561deaaf254e
FernadaGlez <Sge74729@lasallebajio.edu.mx>
2024-09-16 22:47:35 -0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { MongoDatabase } from "./data/init";
 import { IncidentModel } from "./data/models/incident.model";
 import { AppRoutes } from "./presentation/routes";
 import { emailJob } from '../domain/jobs/email.job';
+import mongoose from 'mongoose';
 
 console.log(envs.PORT);
 const app = express();
@@ -24,10 +25,52 @@ app.use(AppRoutes.routes);
 
 
 app.listen(envs.PORT,()=>{
-    console.log("Servidor esta corriendo")
-    emailJob();
+console.log("Corriendo puerto 3000")
 })
 
+const caseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  lng: Number,
+  lat: Number,
+    isEmailSent: Boolean,
+    genre: String,
+    age: Number,
+  date: { type: Date, default: Date.now },
 });
+const Case = mongoose.model('Case', caseSchema);
+
+app.post('/cases', async (req, res) => {
+  const newCase = new Case(req.body);
+  await newCase.save();
+  res.status(201).send(newCase);
+});
+
+app.get('/cases', async (req, res) => {
+  const cases = await Case.find();
+  res.send(cases);
+});
+
+app.get('/cases/lastweek', async (req, res) => {
+  const oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+  const recentCases = await Case.find({ date: { $gte: oneWeekAgo } });
+  res.send(recentCases);
+});
+
+app.put('/cases/:id', async (req, res) => {
+  const caseId = req.params.id;
+  const updatedCase = await Case.findByIdAndUpdate(caseId, req.body, { new: true });
+  res.send(updatedCase);
+});
+
+app.delete('/cases/:id', async (req, res) => {
+  const caseId = req.params.id;
+  await Case.findByIdAndDelete(caseId);
+  res.json({ message: 'Caso eliminado' });
+});
+
+
+
 
 
